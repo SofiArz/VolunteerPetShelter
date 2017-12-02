ea18763eefee93c94829d6b47ec8778c2900f2a6
SofiArz <sofia.arzamendia@hotmail.es>
2017-12-02 19:03:15 -0300

    Creacion Menu administrador

---
+++ VolunteerPetShelter/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Optimization;
+
+namespace VolunteerPetShelter
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+               .Include(
+                "~/Content/vendor/bootstrap/css/bootstrap.min.css",          //--Bootstrap Core CSS--
+                "~/Content/vendor/metisMenu/metisMenu.min.css",               //--MetisMenu CSS-- 
+                "~/Content/dist/css/sb-admin-2.css",                          //--Custom CSS-- 
+                "~/Content/vendor/morrisjs/morris.css",                //--Morris Charts CSS --
+                "~/Content/vendor/font-awesome/css/font-awesome.min.css"));   //--Custom Fonts-
+
+
+                   bundles.Add(new ScriptBundle("~/bundles/js")
+              .Include(
+                "~/Content/vendor/jquery/jquery.min.js",           //--jQuery-- 
+                "~/Content/vendor/bootstrap/js/bootstrap.min.js",  //--Bootstrap Core JavaScript--
+                "~/Content/vendor/metisMenu/metisMenu.min.js",     //--Metis Menu Plugin JavaScript-- 
+                "~/Content/vendor/raphael/raphael.min.js",         //--Morris Charts JavaScript--
+                "~/Content/vendor/morrisjs/morris.min.js",
+                "~/Content/data/morris-data.js",
+                "~/Content/dist/js/sb-admin-2.js"));               //--Custom Theme JavaScript--
+
+
+        }
+    }
+}
---
+++ VolunteerPetShelter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VolunteerPetShelter.MvcApplication" Language="C#" %>
